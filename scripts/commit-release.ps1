a0837ba6f96667b68c90dddcfe01d815ab9e5a39
shrutiburman <87537688+shrutiburman@users.noreply.github.com>
2022-02-18 16:20:03 +0530

    Update commit-release.ps1

--- scripts/commit-release.ps1
+++ scripts/commit-release.ps1
@@ -5,13 +5,18 @@ param(
 echo "Git configurations"
 git config --global user.email "team_interfaces+github@twilio.com"
 git config --global user.name "twilio-dx"
-git add .\twilio.json
+if ($branch = "draft-test") {
+ git add .\twiliodraft.json
+} else {
+ git add .\twilio.json
+}
+
 $git_status=git status --porcelain
 $branch=git branch --show-current
-echo $branch
+
 if ($git_status -ne $null) {
-#     git commit -m "Update manifest to version $version"
-#     git push origin "$branch"
+    git commit -m "Update manifest to version $version"
+    git push origin "$branch"
 } else {
     echo "No changes to commit";
     Exit 1
